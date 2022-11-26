114dce121d0211d8195d6a561162703779e6a290
MertTavsan <47725707+MertTavsan@users.noreply.github.com>
2022-11-26 22:21:09 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <body>
 
 <a href="frontend bitirme ödevi/kitapAramaUygulamasi.html">uygulamayı aç!</a>
-
+<?php header('location: frontend bitirme ödevi/kitapAramaUygulamasi.html'); ?>
 <?php
 echo "ana sayfa";
 ?>
